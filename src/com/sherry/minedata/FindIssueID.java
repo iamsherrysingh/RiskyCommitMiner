3de0f6af25d5ef65f9070e61902e8bc3866e17e5
iamsherrysingh <iamsherrysingh@gmail.com>
2020-02-27 23:47:08 -0500

    added getListOfIssues

---
+++ src/com/sherry/minedata/FindIssueID.java
@@ -0,0 +1,13 @@
+package com.sherry.minedata;
+
+import java.util.List;
+
+public class FindIssueID {
+	
+	public List<String> findCommits(String jsonResultAffectedVersions) {
+		
+		
+	return null;
+	}
+
+}
--- src/com/sherry/rest/RestClient.java
+++ src/com/sherry/rest/RestClient.java
@@ -33,4 +33,32 @@ public class RestClient {
         
         return output;
     }
+    
+    public String getListOfIssues(String project, String affectedVersion) {
+    	project="MATH";
+    	affectedVersion="3.6";
+    	String output="";
+        try {
+
+            URL url = new URL("https://issues.apache.org/jira/rest/api/2/search?jql=project="+project+"+AND+type=bug+and+affectedVersion="+affectedVersion);//your url i.e fetch data from .
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP Error code : "
+                        + conn.getResponseCode());
+            }
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+            BufferedReader br = new BufferedReader(in);
+//            while ((output = br.readLine()) != null) {
+//                System.out.println(output);
+//            }
+            conn.disconnect();
+
+        } catch (Exception e) {
+            System.out.println("Exception in getCommentsForIssue:- " + e);
+        }
+        
+        return output;
+    }
 }
